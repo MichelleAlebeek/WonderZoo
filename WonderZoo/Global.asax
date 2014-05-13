a5dd74fbaf9d755b0fecf595a84069138e739175
MichelleAlebeek <m.alebeek@student.fontys.nl>
2014-05-13 13:21:23 +0200

    Opstart

---
+++ WonderZoo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WonderZoo.Global" Language="C#" %>
---
+++ WonderZoo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WonderZoo.Startup))]
+namespace WonderZoo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
