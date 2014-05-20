27983a25a53b7a5ebd64790c110b230a17ed7341
MichelleAlebeek <m.alebeek@student.fontys.nl>
2014-05-20 21:01:13 +0200

    Databasekoppeling toegevoegd

---
+++ Databasekoppeling/Databasekoppeling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using Oracle.DataAccess;
+using System.Configuration;
+using Databasekoppeling.Properties;
+using System.Configuration;
+
+namespace Databasekoppeling
+{
+    public class Databasekoppeling
+    {
+        private static OracleConnection connection;
+
+        public Databasekoppeling()
+        {
+           
+           // string connectie = Settings.Default.ConnectieString;
+           // connection = new OracleConnection(connectie);
+        }
+
+        public void MethodeNaam()
+        {
+
+        }
+    }
+}
