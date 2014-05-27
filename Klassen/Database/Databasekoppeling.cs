d859743b434e3ea645ee1c77e10679fd4755a4a6
MichelleAlebeek <m.alebeek@student.fontys.nl>
2014-05-27 21:54:25 +0200

    Login methode met database laten communiceren

--- Klassen/Database/Databasekoppeling.cs
+++ Klassen/Database/Databasekoppeling.cs
@@ -9,22 +9,39 @@ using Oracle.DataAccess;
 using System.Configuration;
 using System.Configuration;
 using Klassen.Properties;
+using Klassen;
 
 namespace Databasekoppeling
 {
     public class Databasekoppeling
     {
-        private static OracleConnection connection;
+        private string connectie;
 
         public Databasekoppeling()
         {    
-            string connectie = Settings.Default.ConnectieString;
-            connection = new OracleConnection(connectie);
+            connectie = Settings.Default.ConnectieString;
+          //  connection = new OracleConnection(connectie);
         }
 
-        //public void MethodeNaam()
-        //{
+        public Persoon Login(string username, string wachtwoord)
+        {
+            using (OracleConnection conn = new OracleConnection(connectie))
+            {
+                OracleCommand cmd = new OracleCommand("select * from gebruiker where gebruikersnaam=:naam and wachtwoord=:wachtwoord", conn);
+                
+                cmd.Parameters.Add("naam", username);
+                cmd.Parameters.Add("wachtwoord", wachtwoord);
 
-        //}
+                OracleDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.Read())
+                {
+                    Persoon g = new Persoon(rdr["naam"].ToString(), Convert.ToInt32(rdr["leeftijd"]), rdr["wachtwoord"].ToString());
+                    return g;
+                }
+                return null;
+            }
+
+        }
     }
 }
