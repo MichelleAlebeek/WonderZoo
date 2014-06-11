ad92407821ba7e4d9c874c49a6b630b64d449657
MichelleAlebeek <m.alebeek@student.fontys.nl>
2014-06-11 10:37:37 +0200

    Form dierenarts gemaakt, methodes aan form gehangen (moet nog getest worden)

--- WonderZooWeb/Dierenarts.aspx
+++ WonderZooWeb/Dierenarts.aspx
@@ -2,6 +2,23 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainInhoud" runat="server">
+    &nbsp;
+    <br />
+&nbsp;
+    <asp:Button ID="BtnMedicijnen" runat="server" Text="Medicijnen" OnClick="BtnMedicijnen_Click" />
+    <asp:Button ID="BtnDuurMedicijn" runat="server" Text="Duur van medicijn" OnClick="BtnDuurMedicijn_Click" />
+    <asp:Button ID="BtnZiektes" runat="server" Text="Veel voorkomende ziektes" OnClick="BtnZiektes_Click" />
+    <br />
+    &nbsp; Diernummer:
+    <asp:TextBox ID="TxtDierNr" runat="server"></asp:TextBox>
+&nbsp;Diernaam:
+    <asp:TextBox ID="TxtDiernaam" runat="server"></asp:TextBox>
+&nbsp;<asp:TextBox ID="TxtDuurMedicijn" runat="server"></asp:TextBox>
+&nbsp;Diersoortnaam:
+    <asp:TextBox ID="TxtDiersoortnaam" runat="server"></asp:TextBox>
+    <br />
+    &nbsp;
+    <asp:ListBox ID="ListBox1" runat="server" Height="231px" Width="767px"></asp:ListBox>
 </asp:Content>
 <asp:Content ID="Content3" runat="server" contentplaceholderid="MainImage">
                  <asp:Image ID="ImageDierenarts" runat="server" Height="97px" ImageAlign="Right" Width="385px" style="margin-left: 2px" ImageUrl="~/Images/reuzenmiereneterkop.jpg" />                           
--- WonderZooWeb/Dierenarts.aspx.cs
+++ WonderZooWeb/Dierenarts.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Klassen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,44 @@ namespace WonderZooWeb
 {
     public partial class Dierenarts : System.Web.UI.Page
     {
+        Beheer beheerder;
         protected void Page_Load(object sender, EventArgs e)
         {
+            beheerder = new Beheer();
+        }
 
+        protected void BtnMedicijnen_Click(object sender, EventArgs e)
+        {
+            int diernummer = Convert.ToInt32(TxtDierNr.Text);
+            List<Medicijn> medicijnen = beheerder.MedicijnVanDier(diernummer);
+
+            foreach (Medicijn med in medicijnen)
+            {
+                ListBox1.Items.Add(med.ToString());
+            }          
+        }
+
+        protected void BtnDuurMedicijn_Click(object sender, EventArgs e)
+        {
+            string diernaam = TxtDiernaam.Text;
+            DateTime startdatum = beheerder.MedicijnStartdatum(diernaam);
+            int duurmedicijn = Convert.ToInt32((startdatum - DateTime.Today));
+            TxtDuurMedicijn.Text = Convert.ToString(duurmedicijn);
+        }
+
+        protected void BtnZiektes_Click(object sender, EventArgs e)
+        {
+            string diersoortnaam = TxtDiersoortnaam.Text;
+            int diersoortnummer;
+            foreach (Diersoort diersoort in beheerder.Diersoorten)
+            {
+                if (diersoort.Familie == diersoortnaam)
+                {
+                    diersoortnummer = diersoort.Diersoortnummer;
+                    string ziektes = beheerder.VeelVoorkomendeZiektesDiersoort(diersoortnummer);
+                    ListBox1.Items.Add(ziektes);
+                }
+            }          
         }
     }
 }
